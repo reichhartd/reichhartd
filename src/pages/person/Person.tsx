4616f137f05b9cf9d75a62494107eb21b768b1a8
Daniel Reichhart <daniel.reichhart@gmx.net>
2020-11-21 16:15:37 +0100

    Simple backend call

--- src/pages/person/Person.tsx
+++ src/pages/person/Person.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Person = () => <h1>Person</h1>;
+export enum HealthStatus {
+    UP = 'UP',
+    DOWN = 'DOWN',
+}
+
+const Person = () => {
+    const [data, setData] = useState(HealthStatus.DOWN);
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('/.well-known/live');
+            const data = await response.json();
+            setData(data.status);
+        })();
+    });
+
+    return (
+        <>
+            <h1>Backend running:</h1>
+            <h2>Status: {data}</h2>
+        </>
+    );
+};
 
 export default Person;
